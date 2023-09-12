396fe02bb6de2b2314b52f0e294c5b8680f08e8a
Allanbrotre <123261565+Allanbrotre@users.noreply.github.com>
2023-09-11 23:42:38 -0600

    Nuevo 2

--- HojaDeTrabajo01/Ejercicio02.h
+++ HojaDeTrabajo01/Ejercicio02.h
@@ -10,26 +10,3 @@ public:
 	bool isPalindrome(string &s);
 };
 
-bool    Ejercicio02::isPalindrome(string& pa)
-{
-    int a = 0;
-    int aa = pa.length() - 1;
-
-    int con = 0;
-    for (int i = 0; i < pa.length();i++)
-    {
-        for (int x = 0;x < pa.size();x++)
-        {
-            if (pa[a] == pa[aa])
-            {
-                a++;
-                aa--;
-            }
-            else
-            {
-                return false;
-            }
-        }
-    }
-    return true;
-}
